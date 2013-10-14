e00f6d16d9e361caa431b1e1a55ddc13b8da7b4f
mstiles92 <mstiles92@gmail.com>
2013-10-14 12:42:04 -0400

    Use explicit import statements

--- src/com/mcprohosting/plugins/imraising/JSONHandler.java
+++ src/com/mcprohosting/plugins/imraising/JSONHandler.java
@@ -1,10 +1,14 @@
 package com.mcprohosting.plugins.imraising;
 
-import org.json.JSONObject;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+
+import org.json.JSONObject;
 
 public class JSONHandler {
 	public static JSONObject readJsonFromUrl(String url) {
