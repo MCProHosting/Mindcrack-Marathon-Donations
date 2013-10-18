4d9ec7e272ebf2a16cc955eb2fb0feff5c872156
MattSalsamendi <matt@mcprohosting.com>
2013-10-18 09:42:34 -0400

    Update Whitelist handler for new API

--- src/com/mcprohosting/plugins/imraising/WhitelistHandler.java
+++ src/com/mcprohosting/plugins/imraising/WhitelistHandler.java
@@ -8,29 +8,24 @@ import org.json.simple.JSONObject;
 
 
 public class WhitelistHandler {
-
 	public static void whitelistFromJSON(JSONObject jsonObject) {
-		JSONArray donationJSON = (JSONArray) jsonObject.get("donation");
+		JSONArray donationJSON = (JSONArray) jsonObject.get("donations");
 		
 		for (Object o : donationJSON) {
 			JSONObject donation = (JSONObject) o;
 			
-			String ign = (String) donation.get("custom");
+			String ign = donation.get("comments").toString().toLowerCase().trim();
 
 			OfflinePlayer player = Bukkit.getOfflinePlayer(ign);
 			
 			if (!player.isWhitelisted()) {
 				Number donationAmount;
-				donationAmount = (Number) donation.get("amount");
+				donationAmount =  Integer.parseInt(donation.get("amount").toString());
 				
 				if (donationAmount.floatValue() >= 20f) {
-					String screenName = (String) donation.get("screen");
-					String message = (String) donation.get("comment");
-					
 					player.setWhitelisted(true);
 					
-					Bukkit.broadcastMessage(ChatColor.GREEN + "Thank you to " + ChatColor.YELLOW + screenName + ChatColor.GREEN + " for donating " + ChatColor.YELLOW + "$" + donationAmount + ChatColor.GREEN + "!");
-					Bukkit.broadcastMessage(ChatColor.GREEN + message);
+					Bukkit.broadcastMessage(ChatColor.GREEN + "Thank you to " + ChatColor.YELLOW + ign + ChatColor.GREEN + " for donating " + ChatColor.YELLOW + "$" + donationAmount + ChatColor.GREEN + "!");
 				}
 			}
 		}
